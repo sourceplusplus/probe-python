45c96eb319124c943101e370d545143d420ea229
Brandon Fergerson <bfergerson@apache.org>
2021-11-15 14:20:24 -0500

    bump

--- sourceplusplus/__init__.py
+++ sourceplusplus/__init__.py
@@ -1,7 +1,7 @@
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __name__ = 'Source++'
 agent_name = 'Source++ Python Probe'
 
 __version_major__ = '0'
 __version_minor__ = '1'
-__version_micro__ = '1'
+__version_micro__ = '2'
