69d57185dda1477b053883c08565a263fe961e11
Brandon Fergerson <bfergerson@apache.org>
2021-11-17 17:29:04 -0500

    assembleUp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,32 @@
 plugins {
     id("ru.vyarus.use-python") version "2.3.0"
+    id("com.avast.gradle.docker-compose")
 }
 
 python {
     pip("apache-skywalking:0.7.0")
     pip("vertx-eventbus-client:1.0.0")    
+}
+
+tasks {
+    register<Exec>("buildDist") {
+        commandLine("sh", "-c", "python setup.py sdist")
+    }
+
+    register<Copy>("updateDockerFiles") {
+        dependsOn("buildDist")
+        from("dist/")
+        into("e2e/")
+    }
+
+    register("assembleUp") {
+        dependsOn("updateDockerFiles", "composeUp")
+    }
+    getByName("composeUp").mustRunAfter("updateDockerFiles")
+}
+
+dockerCompose {
+    dockerComposeWorkingDirectory.set(File("./e2e"))
+    removeVolumes.set(true)
+    waitForTcpPorts.set(false)
 }
