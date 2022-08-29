332dbd60cba31c46c9b8249b92d29dc75b729038
Brandon Fergerson <bfergerson@apache.org>
2022-08-29 21:01:52 +0400

    test: extended config

--- tests/probe_config/tests.py
+++ tests/probe_config/tests.py
@@ -29,3 +29,20 @@ class TestSum(unittest.TestCase):
         self.assertEqual("spp-platform:11800", spp.probe_config["skywalking"]["collector"]["backend_service"])
         self.assertEqual("tutorial-jvm", spp.probe_config["skywalking"]["agent"]["service_name"])
         self.assertEqual("WARN", spp.probe_config["skywalking"]["logging"]["level"])
+
+    @mock.patch.dict('os.environ', {
+        'SPP_PROBE_CONFIG_FILE': '%s/resources/extended-config.yml' % os.path.dirname(__file__)
+    })
+    def test_config_from_env_extended(self):
+        spp = SourcePlusPlus()
+        self.assertEqual("spp-platform", spp.probe_config["spp"]["platform_host"])
+        self.assertEqual(12800, spp.probe_config["spp"]["platform_port"])
+        self.assertEqual(False, spp.probe_config["spp"]["quiet_mode"])
+        self.assertEqual(False, spp.probe_config["spp"]["verify_host"])
+        self.assertIsNotNone(spp.probe_config["spp"]["platform_certificate"])
+        self.assertIsNotNone(spp.probe_config["spp"]["probe_id"])
+        self.assertEqual(True, spp.probe_config["spp"]["ssl_enabled"])
+        self.assertEqual("INFO", spp.probe_config["skywalking"]["logging"]["level"])
+        self.assertEqual("spp", spp.probe_config["skywalking"]["agent"]["service_name"])
+        self.assertEqual("spp-platform:11801", spp.probe_config["skywalking"]["collector"]["backend_service"])
+        self.assertEqual(False, spp.probe_config["skywalking"]["plugin"]["toolkit"]["log"]["transmit_formatted"])
